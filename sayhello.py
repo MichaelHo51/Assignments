73247d1f69a237869ba60878eb6f2e72cd883a07
Michael Ho <michaelho.nc@gmail.com>
2018-01-05 16:29:21 -0500

    Add comments to say hello in new branch

--- sayhello.py
+++ sayhello.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+#This simple program asks for user input and then returns it with "Hello" in front
 import sys
+
+#User is prompted to input here 
 name = sys.stdin.read()
+
+#Program spits out hello plus input
 print "Hello " + name + "!"
