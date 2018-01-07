ce3083f3d23d603a7e58e9a9c30a81657687a1cc
Michael Ho <michaelho.nc@gmail.com>
2018-01-07 12:56:38 -0500

    Punched in some numbers into the calculator to test

--- calculator.py
+++ calculator.py
@@ -10,3 +10,9 @@ def multiplication(x,y):
 def division(x,y):
 	return (x/y)
 
+print "I will use the calculator to add 333 to 777, then divide that result by 10"
+a = addition(333,777)
+b = division(a,10)
+
+print a
+print b
